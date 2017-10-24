c6cc14a01de505b444b800a7ac7671aab1ace308
Alfredo Hernández <aldomann.designs@gmail.com>
2017-10-24 21:26:25 +0200

    Script to compare complexity of different methods to solve the N queen problem

---
+++ genetic-algorithms/n-queens-brute.R
@@ -0,0 +1,36 @@
+# Comparison between brute force methods complexity for the N queen puzzle
+# Author: Alfredo Hernández <aldomann.designs@gmail.com>
+
+library(tidyverse)
+
+# Raw data -------------------------------------------------
+
+n <- c(1, 4, 5, 6, 7, 8, 9, 10)
+sols <- c(1, 1, 2, 1, 6, 12, 46, 92)
+all <- n^n
+perms <- factorial(n)
+
+# Data visualisation ---------------------------------------
+
+# First method (stacked)
+queens.df <- data.frame(cbind(n,sols,all,perms))
+
+ggplot(queens.df, aes(x = as.factor(n))) +
+	geom_bar(aes(y = all, fill = "all"), stat = "identity") +
+	geom_bar(aes(y = perms, fill = "perms"), stat = "identity") +
+	geom_bar(aes(y = sols, fill = "sols"), stat = "identity") +
+	scale_fill_manual(values = c("all" = "brown1", "perms" = "slateblue", "sols" = "limegreen")) +
+	scale_y_log10() +
+	labs(title =  "Comparison of brute force complexity for the N queens puzzle",
+			 x = "Board size (N)", y = "Count", fill = "Type")
+
+# Second method (dodge)
+library(reshape2)
+queens.df2 <- melt(data.frame(cbind(n,all,perms,sols)),id.vars = 1)
+
+ggplot(queens.df2, aes(x = as.factor(n), y = value)) +
+	geom_bar(aes(fill = variable),stat = "identity",position = "dodge") +
+	scale_fill_manual(values = c("all" = "brown1", "perms" = "slateblue", "sols" = "limegreen")) +
+	scale_y_log10() +
+	labs(title =  "Comparison of brute force complexity for the N queens puzzle",
+			 x = "Board size (N)", y = "Count", fill = "Type")
