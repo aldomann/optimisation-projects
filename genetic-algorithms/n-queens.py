320a9258257be351d4a94beaf4edeaa37ece9ac0
Alfredo Hernández <aldomann.designs@gmail.com>
2017-11-20 10:32:42 +0100

    Finished GA subalgorithms

--- genetic-algorithms/n-queens.py
+++ genetic-algorithms/n-queens.py
@@ -21,7 +21,7 @@ import numpy as np
 import sys
 import random
 
-MAX_QUEENS = 10
+MAX_QUEENS = 15
 N_QUEENS = int(sys.argv[1])
 
 if N_QUEENS > MAX_QUEENS:
@@ -29,8 +29,7 @@ if N_QUEENS > MAX_QUEENS:
 	exit()
 
 MAX_FIT = N_QUEENS * (N_QUEENS - 1)
-# MUTATE = 0.000001
-MUTATE = 0.1
+MUTATE = 0.000001
 MUTATE_FLAG = True
 MAX_ITER = 100000
 POPULATION = int(sys.argv[2])
@@ -86,133 +85,63 @@ def get_parent(population):
 
 def mutate(board):
 	# Mutate a board using a mask
-	# TODO: implement MUTATE_FLAG in main algorithm
 	if random.random() < MUTATE :
 		a, b = random.randint(0, N_QUEENS - 1), random.randint(0, N_QUEENS - 1)
 		while (b == a): # To ensure a true mutation
 			b = random.randint(0, N_QUEENS - 1)
 		population[board].sequence[a], population[board].sequence[b] = population[board].sequence[b], population[board].sequence[a]
-		# print("CANCER!", a, "<->", b)
-	# else:
-		# print("No mutation :)")
-
-def pmx_crossover(board_a, board_b):
-	# Choose crossover points
-	cx1 = random.randint(0, N_QUEENS - 1)
-	cx2 = random.randint(0, N_QUEENS - 1)
-	if cx1 > cx2: # Swap the two cx points
-		print ("swapping")
-		cx1, cx2 = cx2, cx1
-	print("cx1:", cx1, "cx2:", cx2)
-	# Apply crossover
-	temp_a = []
-	temp_b = []
-	for i in range(cx1, cx2 + 1):
-		# Keep track of the selected values
-		temp_a.append(board_a[i])
-		temp_b.append(board_b[i])
-
-	for i in range(len(temp_a)):
-		print(temp_a[i], "<->",temp_b[i])
-		# Swap values on board_a
-		board_a[board_a.tolist().index(temp_a[i])], \
-		board_a[board_a.tolist().index(temp_b[i])] = \
-		board_a[board_a.tolist().index(temp_b[i])], \
-		board_a[board_a.tolist().index(temp_a[i])]
-		# Swap values on board_b
-		board_b[board_b.tolist().index(temp_a[i])], \
-		board_b[board_b.tolist().index(temp_b[i])] = \
-		board_b[board_b.tolist().index(temp_b[i])], \
-		board_b[board_b.tolist().index(temp_a[i])]
-
-def uniform_crossover(board_a, board_b, prob=0.1):
-	# Swap individual indexes with probability prob
-	for i in range(N_QUEENS):
-		if random.random() < prob:
-			print(i)
-			board_a[i], board_b[i] = board_b[i], board_a[i]
 
 def ordered_crossover(ind1, ind2):
-	size = N_QUEENS
-	a, b = random.sample(range(size), 2)
+	# Ordered crossover
+	a, b = random.sample(range(N_QUEENS), 2)
 	if a > b:
 			a, b = b, a
-	print(a,b)
-
-	holes1, holes2 = [True]*size, [True]*size
-	for i in range(size):
+	holes1, holes2 = [True]*N_QUEENS, [True]*N_QUEENS
+	for i in range(N_QUEENS):
 		if i < a or i > b:
 			holes1[ind2[i]] = False
 			holes2[ind1[i]] = False
-
+	# We must keep the original values somewhere before scrambling everything
 	temp1, temp2 = ind1, ind2
 	k1 , k2 = b + 1, b + 1
-	for i in range(size):
-		if not holes1[temp1[(i + b + 1) % size]]:
-			ind1[k1 % size] = temp1[(i + b + 1) % size]
+	for i in range(N_QUEENS):
+		if not holes1[temp1[(i + b + 1) % N_QUEENS]]:
+			ind1[k1 % N_QUEENS] = temp1[(i + b + 1) % N_QUEENS]
 			k1 += 1
-
-		if not holes2[temp2[(i + b + 1) % size]]:
-			ind2[k2 % size] = temp2[(i + b + 1) % size]
+		if not holes2[temp2[(i + b + 1) % N_QUEENS]]:
+			ind2[k2 % N_QUEENS] = temp2[(i + b + 1) % N_QUEENS]
 			k2 += 1
-
+	# Swap the content between a and b (included)
 	for i in range(a, b + 1):
 		ind1[i], ind2[i] = ind2[i], ind1[i]
 
-	return ind1, ind2
-
-
 ################################################################
-
-def stop_condition():
-	# It stops the GA when a solution is found
-	# TODO: implement this in main algorithm
-	fitnessvals = [pos.fitness for pos in population]
-	if MAX_FIT in fitnessvals:
-		return True
-	if MAX_ITER == iteration:
-		return True
-	return False
-
-################################################################
-
-def get_good_queens(population = None):
-	good = 0
-	for board in range(len(population)):
-		if ( population[board].fitness == MAX_FIT ):
-				good += 1
-				print( "Q",  board, ":", population[board].sequence, "fitness:", population[board].fitness )
-	print("There are", good, "non-fundamental solutions.")
 
 def find_good_queen(population = None):
-	good = False
+	# Look for a good board (slow as hell)
 	for board in range(len(population)):
 		if ( population[board].fitness == MAX_FIT ):
-			good = True
 			print("Found a non-fundamental solution.")
 			print( "Q",  board, ":", population[board].sequence, "fitness:", population[board].fitness )
+			return True
+
+def GA(MAX_ITER):
+	for iteration in range(MAX_ITER):
+		print(" #"*5 ,"Genetic generation :", iteration, "#"*5)
+		parent1 = get_parent(population)
+		parent2 = get_parent(population)
+		ordered_crossover(population[parent1].sequence, population[parent2].sequence)
+		if(MUTATE_FLAG):
+			mutate(parent1)
+			mutate(parent2)
+		# Update fitness
+		population[parent1].set_fitness(assess_fitness(population[parent1].sequence))
+		population[parent2].set_fitness(assess_fitness(population[parent2].sequence))
+		if find_good_queen(population):
 			break
-	if ( good == False ):
-		print("Couldn't find any solution.")
 
 ################################################################
 
 population = generate_population(POPULATION)
-# get_good_queens(population)
-find_good_queen(population)
-
-# get_parent(population)
-
-# for board in range(len(population)):
-# 	print(population[board].sequence)
-# 	mutate(board)
-# 	print(population[board].sequence)
-# 	print()
-
-print(population[1].sequence)
-print(population[2].sequence)
-# pmx_crossover(population[1].sequence, population[2].sequence)
-# uniform_crossover(population[1].sequence, population[2].sequence)
-ordered_crossover(population[1].sequence, population[2].sequence)
-print(population[1].sequence)
-print(population[2].sequence)
+good = False
+GA(MAX_ITER)
