38845bb7bcec6344dcb4559ae7d5d5552638f68b
Alfredo Hernández <aldomann.designs@gmail.com>
2017-11-25 16:06:24 +0100

    Added script for the parameter control analysis

---
+++ genetic-algorithms/optimise_parameters.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Script to find the "optimal" parameters for the N Queens Problem
+# Author: Alfredo Hernández <aldomann.designs@gmail.com>
+
+
+# Python and its libraries are provided by Anaconda 3
+anacondainit
+
+# Looping over N
+for (( N = 8; N <= 10; N ++ )); do
+	# Looping over POPULATION
+	echo 'Running the Genetic Algorithm for N =' $N
+	for (( P = 50; P <= 50 * N ; P += 50 )); do
+		# Looping over MUTATE_PROB
+		# for (( M = 5; M <= 10; M ++ )); do
+		for M in 0.005 0.006 0.007 0.008 0.009 0.01; do
+			# 100 runs for each tuple
+			for (( i = 0; i < 100; i++ )); do
+				python n-queens.py $N $P $M
+			done
+		done
+	done
+done
