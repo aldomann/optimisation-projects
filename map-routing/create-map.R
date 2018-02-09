b26f8458cf2a3141ce244a6eb40bd9ab20a83d6e
Alfredo Hernández <aldomann.designs@gmail.com>
2018-02-09 13:11:08 +0100

    Updated script to get BCN->UAB map

--- map-routing/create-map.R
+++ map-routing/create-map.R
@@ -6,19 +6,22 @@
 library(tidyverse)
 library(data.table)
 library(ggmap)
-library(maps)
-library(ggalt)
 
 setwd("map-routing/")
 
 # Read solution --------------------------------------------
 solution <- fread("solution.csv")
+solution.uab <- fread("solution-uab.csv")
 
 # Map functions --------------------------------------------
 
 spain <- get_map(location = c(lon = mean(solution$Lon),
 															lat = mean(solution$Lat)),
 								 zoom = 6, maptype = "roadmap", source = "google")
+
+uab <- get_map(location = c(lon = mean(solution.uab$Lon),
+														lat = mean(solution.uab$Lat)),
+							 zoom = 11, maptype = "roadmap", source = "google")
 
 scale_x_longitude <- function(xmin = -180, xmax = 180, step = 1, xtra.lim = 1.5, ...) {
 	xbreaks <- seq(xmin,xmax,step)
@@ -41,12 +44,14 @@ scale_y_latitude <- function(ymin = -90, ymax = 90, step = 0.5, xtra.lim = 1.5, 
 lon.data <- solution$Lon
 lat.data <- solution$Lat
 
+lon.data.uab <- solution.uab$Lon
+lat.data.uab <- solution.uab$Lat
+
 node.start <- 1
 node.goal <- nrow(solution)
+node.goal.uab <- nrow(solution.uab)
 
 map <- ggmap(spain) +
-	scale_x_continuous(limits=c(-6,2.5), expand = c(0.1,0)) +
-	scale_y_continuous(limits=c(36,42), expand = c(0,0)) +
 	geom_path(data=solution, aes(x=Lon, y=Lat),
 						color = "royalblue1", size = 1) +
 	# Starting node
@@ -69,4 +74,31 @@ map <- ggmap(spain) +
 	scale_y_latitude(ymin = floor(min(lat.data)), ymax = ceiling(max(lat.data)),
 									 step = 1, xtra.lim = 0.1)
 
+
+map.uab <- ggmap(uab) +
+	geom_path(data=solution.uab, aes(x=Lon, y=Lat),
+						color = "royalblue1", size = 1) +
+	# Starting node
+	geom_point(aes(x = lon.data.uab[node.start],
+								 y = lat.data.uab[node.start]),
+						 colour = "lightsalmon", size = 8, alpha =0.1) +
+	geom_point(aes(x = lon.data.uab[node.start],
+								 y = lat.data.uab[node.start]),
+						 colour = "white", fill = "lightsalmon", stroke = 1, shape=21, size = 5) +
+	# Goal node
+	geom_point(aes(x = lon.data.uab[node.goal.uab],
+								 y = lat.data.uab[node.goal.uab]),
+						 colour = "lightsalmon", size = 8, alpha =0.1) +
+	geom_point(aes(x = lon.data.uab[node.goal.uab],
+								 y = lat.data.uab[node.goal.uab]),
+						 colour = "white", fill = "lightsalmon", stroke = 1, shape=21, size = 5) +
+	labs(x = "Longitude", y = "Latitude") +
+	scale_x_longitude(xmin = floor(min(lon.data.uab)), xmax = ceiling(max(lon.data.uab))-0.7,
+										step = 0.1, xtra.lim = 0) +
+	scale_y_latitude(ymin = floor(min(lat.data.uab))+0.35, ymax = ceiling(max(lat.data.uab))-0.45,
+									 step = 0.05, xtra.lim = 0)
+
 map + theme_bw() #+ theme(text = element_text(family = "LM Roman 10")) + ggsave(filename = "solution.pdf", width = 6, height = 4, dpi = 96, device = cairo_pdf)
+
+map.uab + theme_bw() #+ theme(text = element_text(family = "LM Roman 10")) + ggsave(filename = "solution-uab.pdf", width = 5, height = 4, dpi = 96, device = cairo_pdf)
+
